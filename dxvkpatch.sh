15ada7bb5bd43f9100af177eaafcec6e51f03ac6
RosieZiutka <129593622+RosieZiutka@users.noreply.github.com>
2023-04-01 10:58:51 +0000

    Update dxvkpatch.sh
    Add [Y/N] Segment

--- dxvkpatch.sh
+++ dxvkpatch.sh
@@ -13,12 +13,20 @@ echo '[0/2] Applying DXVK patch.'
 echo '[1/2] [0/1] Downloading packages. [⟱]'
 if [ ! -d "$WINESTEAM_PKGS" ]; then mkdir -p "$WINESTEAM_PKGS"; fi
 cd "$WINESTEAM_PKGS"
-if [ ! -d ./dxvk-2.0 ]; then
-  echo '[1/2] [1/1] Downloading DXVK... [⟱]'
-  wget https://github.com/doitsujin/dxvk/releases/download/v2.0/dxvk-2.0.tar.gz
-  tar -xvzf dxvk-2.0.tar.gz
+read -p "Are you sure? Some people might not need the patch. [N/Y] " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    if [ ! -d ./dxvk-2.0 ]; then
+   echo '[1/2] [1/1] Downloading DXVK... [⟱]'
+   wget https://github.com/doitsujin/dxvk/releases/download/v2.0/dxvk-2.0.tar.gz
+   tar -xvzf dxvk-2.0.tar.gz
   rm dxvk-2.0.tar.gz
+   fi
+else exit
 fi
+
+
 echo '[2/2] Running DXVK patch... [◌𝨙]'
 bash ./dxvk-2.0/setup_dxvk.sh install
 echo 'Done.'
