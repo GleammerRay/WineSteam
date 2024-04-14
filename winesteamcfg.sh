61fc22fcbbae4e2b9b8005116f64a6d75498d291
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2024-04-14 23:25:11 +0100

    Update winesteamcfg.sh

--- winesteamcfg.sh
+++ winesteamcfg.sh
@@ -1,8 +1,9 @@
 export WINESTEAM_DATA="$HOME/.winesteam"
+export WINESTEAM_PKGS="$WINESTEAM_DATA/packages"
 export WINEPREFIX="$WINESTEAM_DATA/prefix"
 if [ ! -d "$WINEPREFIX" ]; then
   echo 'F:Wineprefix not found.'
   echo 'Run "bash winesteam.sh" to set up your prefix first.'
   exit
 fi
-winecfg
+"$WINESTEAM_PKGS/lutris-GE-Proton8-26-x86_64/bin/winecfg"
