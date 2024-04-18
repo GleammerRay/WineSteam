9a38776a6dac0f7ba9f3f5b98e366f7e4230942c
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2024-04-18 15:05:44 +0100

    Revert "Update winesteam.sh"
    This reverts commit 79ee725bac43762e30bfecbc9cab088a6f8f1205.

--- winesteam.sh
+++ winesteam.sh
@@ -163,7 +163,7 @@ if [ "x$FLATPAK_ID" != "xio.github.gleammerray.WineSteam" ]; then
     echo "Running WineSteam flatpak."
     unshare --user --map-current-user --net --mount "$WINESTEAM_BIN/ws_flatpak_runner.sh" $1 &
     export WS_RUNNER_PID=$!
-    sleep 2
+    sleep 1
     slirp4netns --configure --mtu=65520 --disable-host-loopback $WS_RUNNER_PID tap0 &
     wsCleanup
     exit
@@ -193,7 +193,7 @@ if [ -d "$WINEPREFIX" ]; then
     unshare --user --map-root-user --net --mount "$WINESTEAM_BIN/ws_runner.sh" "wine \"$WINEPREFIX/drive_c/Program Files (x86)/Steam/steam.exe\" -silent" &
   fi
   export WS_RUNNER_PID=$!
-  sleep 2
+  sleep 1
   if [ "x$FLATPAK_ID" != "xio.github.gleammerray.WineSteam" ]; then
     slirp4netns --configure --mtu=65520 --disable-host-loopback $WS_RUNNER_PID tap0 &
   fi
@@ -283,7 +283,7 @@ if [ "x$WINESTEAM_INSTALL_DXVK" = "x" ]; then
       echo "WINESTEAM_INSTALL_MODE=\"flatpak\"" >> "$WINESTEAM_CFG"
       unshare --user --map-current-user --net --mount "$WINESTEAM_BIN/ws_flatpak_runner.sh" &
       export WS_RUNNER_PID=$!
-      sleep 2
+      sleep 1
       ./bin/slirp4netns --configure --mtu=65520 --disable-host-loopback $WS_RUNNER_PID tap0 &
       wsCleanup
       exit
@@ -417,7 +417,7 @@ else
   unshare --user --map-root-user --net --mount "$WINESTEAM_BIN/ws_runner.sh" "wine \"$WINESTEAM_PKGS/SteamSetup.exe\"" &
 fi
 export WS_RUNNER_PID=$!
-sleep 2
+sleep 1
 if [ "x$FLATPAK_ID" != "xio.github.gleammerray.WineSteam" ]; then
   slirp4netns --configure --mtu=65520 --disable-host-loopback $WS_RUNNER_PID tap0 &
 fi
