280a1d9a36984c6b156a439f280e29c176793986
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2024-04-18 14:47:51 +0100

    Update ws_flatpak_runner.sh

--- ws_flatpak_runner.sh
+++ ws_flatpak_runner.sh
@@ -4,4 +4,4 @@ echo $$ > "$WINESTEAM_FLATPAK_PID_PATH"
 sleep 6
 printf "nameserver 8.8.8.8\nnameserver 4.4.4.4\n" > "$WINESTEAM_DATA/resolv.conf"
 mount --bind "$WINESTEAM_DATA/resolv.conf" /etc/resolv.conf
-flatpak run io.github.gleammerray.WineSteam
+flatpak run io.github.gleammerray.WineSteam $1
