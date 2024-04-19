083335718ca7897698a6b35d9ee33dc232cdf94b
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2024-04-20 00:14:46 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -17,17 +17,17 @@ if [ "x$1" = "xflatpak" ]; then
   wsNotify '[2/2] Installing Winesteam flatpak... '
   flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
   flatpak uninstall --user -y io.github.gleammerray.WineSteam
-      flatpak install --user -y org.freedesktop.Platform/x86_64/23.08 \
-          org.freedesktop.Platform.GL32.default/x86_64/23.08 \
-          org.freedesktop.Platform.VAAPI.Intel/x86_64/23.08 \
-          org.freedesktop.Platform.VAAPI.Intel.i386/x86_64/23.08 \
-          org.freedesktop.Platform.ffmpeg-full/x86_64/23.08 \
-          org.freedesktop.Platform.ffmpeg_full.i386/x86_64/23.08 \
-          org.winehq.Wine.gecko/x86_64/stable-23.08 \
-          org.winehq.Wine.mono/x86_64/stable-23.08 \
-          org.winehq.Wine.DLLs.dxvk/x86_64/stable-23.08 \
-          org.freedesktop.Platform.Compat.i386/x86_64/23.08 \
-          org.freedesktop.Platform.Compat.i386.Debug/x86_64/23.08
+  flatpak install --user -y org.freedesktop.Platform/x86_64/23.08 \
+      org.freedesktop.Platform.GL32.default/x86_64/23.08 \
+      org.freedesktop.Platform.VAAPI.Intel/x86_64/23.08 \
+      org.freedesktop.Platform.VAAPI.Intel.i386/x86_64/23.08 \
+      org.freedesktop.Platform.ffmpeg-full/x86_64/23.08 \
+      org.freedesktop.Platform.ffmpeg_full.i386/x86_64/23.08 \
+      org.winehq.Wine.gecko/x86_64/stable-23.08 \
+      org.winehq.Wine.mono/x86_64/stable-23.08 \
+      org.winehq.Wine.DLLs.dxvk/x86_64/stable-23.08 \
+      org.freedesktop.Platform.Compat.i386/x86_64/23.08 \
+      org.freedesktop.Platform.Compat.i386.Debug/x86_64/23.08
   flatpak install --user -y ./WineSteam.flatpak
   rm WineSteam.flatpak
   exit
